f02a2be9380dc4208cc04c89ebceebc21237fed4
likui628 <90845831+likui628@users.noreply.github.com>
2023-11-25 20:39:45 +0800

    chore: test prisma

---
+++ prisma/migrations/20231125123704_init/migration.sql
@@ -0,0 +1,4 @@
+-- CreateTable
+CREATE TABLE "Tags" (
+    "value" TEXT NOT NULL PRIMARY KEY
+);
--- src/app/api/tags/route.ts
+++ src/app/api/tags/route.ts
@@ -1,5 +1,9 @@
-import {NextRequest, NextResponse} from "next/server";
+import { NextResponse} from "next/server";
+import {prisma} from "@/utils/connect";
 
-export const GET = async (req: NextRequest) => {
-    return new NextResponse(JSON.stringify({tags: ['a', 'b']}));
-};
+export const GET = async () => {
+    const tags= await prisma.tags.findMany();
+    return NextResponse.json({
+        tags: tags.map(tag => tag.value)
+    });
+}
