0eb5950b0012d326d73096282f3689f336b84dd6
likui628 <90845831+likui628@users.noreply.github.com>
2023-11-27 21:38:47 +0800

    chore: jwt token

---
+++ src/app/api/utils.ts
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "token secret key";
+
+const TOKEN_ALG = "HS256";
+
+const TOKEN_PREFIX = "Bearer ";
+
+interface AuthPayload {
+  sub: number;
+  user: string;
+}
+
+export function issueToken(payload: AuthPayload) {
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+    algorithm: TOKEN_ALG,
+    expiresIn: "2d",
+  });
+}
+
+function verifyToken<T>(token: string) {
+  const payload = jwt.verify(token, ACCESS_TOKEN_SECRET, { algorithms: [TOKEN_ALG] });
+  return payload as T;
+}
+
+export function loadCurrentUser(authorization: string) {
+  if (!authorization || !authorization.startsWith(TOKEN_PREFIX)) {
+    return;
+  }
+  const token = authorization.split(TOKEN_PREFIX)[1];
+  const payload = verifyToken<AuthPayload>(token);
+  return payload.sub;
+}
