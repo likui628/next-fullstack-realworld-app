2e990782739945e6113f4f689a28b684542ffe12
likui628 <90845831+likui628@users.noreply.github.com>
2023-11-28 11:51:14 +0800

    feat: login

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,16 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/utils/connect";
+import { issueToken } from "@/app/api/utils";
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
-  console.log(body);
+  const user = await prisma.user.findUnique({ where: { email: body.user.email } });
+  if (!user) {
+    return NextResponse.json(
+      {
+        errors: ["User not found"],
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+  const { id, username, password } = user;
+  if (body.user.password !== password) {
+    return NextResponse.json(
+      {
+        errors: ["username or password not correct"],
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+
+  const payload = { sub: id, user: username };
 
   return NextResponse.json({
     user: {
-      email: "",
-      username: "",
-      bio: "",
-      image: "",
-      token: "",
+      ...user,
+      token: issueToken(payload),
     },
   });
 };
--- src/components/user/LoginForm.tsx
+++ src/components/user/LoginForm.tsx
@@ -1,25 +1,51 @@
+"use client";
 import ListErrors from "@/components/common/ListErrors";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/providers/AuthProvider";
 
 const LoginForm = () => {
-  // const data = await fetch('http://localhost:3000/api/users/login', {
-  //     method: 'POST',
-  //     headers: {
-  //         'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //         "user": {
-  //             "email": "alice@realworld.com",
-  //             "password": "alice123"
-  //         }
-  //     })
-  // }).then(res => res.json())
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<string[]>([]);
 
-  // console.log(data)
+  const { login } = useAuth();
+  const router = useRouter();
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const resp = await fetch("http://localhost:3000/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: {
+            email,
+            password,
+          },
+        }),
+      });
+
+      const data = await resp.json();
+      if (resp.ok) {
+        login(data.user.token);
+        router.push("/");
+      } else {
+        setErrors(data.errors);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
-      <ListErrors />
-      <form>
+      <ListErrors errors={errors} />
+      <form onSubmit={handleFormSubmit}>
         <fieldset className="form-group">
           <input
             className="form-control form-control-lg"
@@ -27,6 +53,9 @@ const LoginForm = () => {
             name="email"
             placeholder="Email"
             data-testid="input-email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            disabled={isLoading}
           />
         </fieldset>
         <fieldset className="form-group">
@@ -36,11 +65,15 @@ const LoginForm = () => {
             name="password"
             placeholder="Password"
             data-testid="input-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={isLoading}
           />
         </fieldset>
         <button
           className="btn btn-lg btn-primary pull-xs-right"
           data-testid="btn-submit"
+          disabled={isLoading}
         >
           Sign in
         </button>
