699a22fbc96d23d900f9b66402f332bd86d753db
likui628 <90845831+likui628@users.noreply.github.com>
2023-12-19 14:45:53 +0800

    chore: revalidate follow related path

--- src/app/api/profiles/[username]/follow/route.ts
+++ src/app/api/profiles/[username]/follow/route.ts
@@ -3,9 +3,15 @@ import { prisma } from '@/libs/prisma'
 import { ApiResponse } from '@/app/api/response'
 import getCurrentUser from '@/actions/getCurrentUser'
 import { userMapper } from '@/app/api/mapper'
+import { revalidatePath } from 'next/cache'
 
 interface IParams {
   username: string
+}
+
+function revalidate() {
+  revalidatePath(`/article/[slug]`, 'page')
+  revalidatePath(`/profile/[username]`, 'page')
 }
 
 export const POST = async (
@@ -46,7 +52,7 @@ export const POST = async (
       },
     },
   })
-
+  revalidate()
   return ApiResponse.ok({ profile: userMapper(profile, true) })
 }
 
@@ -83,6 +89,6 @@ export const DELETE = async (
       },
     },
   })
-
+  revalidate()
   return ApiResponse.ok({ profile: userMapper(profile, false) })
 }
